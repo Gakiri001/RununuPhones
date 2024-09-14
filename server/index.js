775be6699c84eaa3b472f7ab2e1100c6f02ff438
Gakiri001 <gakirijulius@gmail.com>
2024-09-14 11:45:28 +0300

    cors array frontend and backend connection

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5174","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
